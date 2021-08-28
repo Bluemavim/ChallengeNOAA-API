f9fee8aa03621a0b8a2cf8284aa1d4cc453de7e0
m.avilano@gmail.com <m.avilano@gmail.com>
2021-08-27 22:29:45 -0300

    Se crea el response para el endpoint que permite listar muestras por color.

---
+++ src/main/java/ar/com/ada/api/monitor/models/response/ListaMuestraColor.java
@@ -0,0 +1,12 @@
+package ar.com.ada.api.monitor.models.response;
+
+import java.sql.Date;
+
+public class ListaMuestraColor {
+    public Integer boyaId;
+    public Date horario;
+    public Integer alturalNivelDelMar;
+
+
+  
+}
