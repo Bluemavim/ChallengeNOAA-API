81fdcf62c24f15f1a120d012af97feb954e792fa
m.avilano@gmail.com <m.avilano@gmail.com>
2021-09-08 22:32:39 -0300

    Se crean tests unitarios . A su vez, se agrega la clase UbicacionesUtils.java para alojar las validaciones requeridas en la consigna.

---
+++ src/main/java/ar/com/ada/api/monitor/utils/UbicacionesUtils.java
@@ -0,0 +1,43 @@
+package ar.com.ada.api.monitor.utils;
+
+public class UbicacionesUtils {
+
+    public static boolean validarLatitud(double latitud) {
+        if (Math.abs(latitud) >= 0 && Math.abs(latitud) <= 90) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+    public static boolean validarLongitud(double longitud) {
+        if (Math.abs(longitud) <= 180) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public static boolean validarCoordenadas(double longitud, double latitud) {
+        if (validarLatitud(latitud) && validarLongitud(longitud)) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+    public static boolean validarCoordenadasFueraDelPlaneta(double longitud, double latitud) {
+        return !validarCoordenadas(longitud, latitud);
+
+    }
+
+    public static boolean validarSiEsHemisferioNorte(double latitud, double longitud){
+        if (!validarCoordenadas(longitud, latitud))
+            return false;
+        if(latitud>=0 && latitud<=90) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+}
